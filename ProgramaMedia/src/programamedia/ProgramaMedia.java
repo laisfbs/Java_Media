5e96980b0eab10737fc517b2db7d82f1a05f0979
Lais <91293868+laisfbs@users.noreply.github.com>
2022-11-17 14:47:14 -0300

    Add files via upload

---
+++ ProgramaMedia/src/programamedia/ProgramaMedia.java
@@ -0,0 +1,28 @@
+
+package programamedia;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author 40415873
+ */
+public class ProgramaMedia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Scanner teclado = new Scanner(System.in);
+        float n1 = teclado.nextFloat();
+        float n2 = teclado.nextFloat();
+        float m = (n1+n2)/2;
+        System.out.println("Sua mediafoi " + m);
+        if (m>9) {
+            System.out.println("Parabens");
+            
+        })
+   }
+    
+}
